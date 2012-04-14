0a217bca0f28ef5d83ff2ce0ae1e76f04dbbd971
Greg Zavitz <gzavitz@gmail.com>
2012-04-14 18:27:18 -0400

    fixed compile error in timeofday probability

--- TrendForecaster/src/probability/TimeOfDayProbability.java
+++ TrendForecaster/src/probability/TimeOfDayProbability.java
@@ -1,5 +1,25 @@
 package probability;
 
-public class TimeOfDayProbability {
+import collections.Hits;
+import dtos.HitDto;
+import dtos.VenueDto;
 
+public class TimeOfDayProbability extends Probability {
+
+	public TimeOfDayProbability(VenueDto venue, int startHour, int stopHour) {
+		super(venue);
+	}
+
+	@Override
+	public float getProbability(Hits hits) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean matches(HitDto hit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
